eefe135da7d330f560cbf6ea10d77e1080f9409f
angelfcerchi <Sorete2340>
2023-05-25 20:37:53 -0300

    Feat: se agrega redes sociales al nav

--- src/components/HeaderComponent/Nav.jsx
+++ src/components/HeaderComponent/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import icon from './assets/nav-icon.png'
+import denver_icon from './assets/nav-icon.png';
 
 const Nav = () => {
     return (
@@ -7,27 +7,40 @@ const Nav = () => {
             <div className='container-fluid facilidades d-none d-sm-block'>
                 <p>Hasta 6 cuotas sin interés. Compras en Efectivo o Transferencia 15% off</p>
             </div>
-            <nav class="navbar navbar-expand-lg p-2">
-                <div class="container-fluid">
-                    <div className="col">
-                        <a href="#"><img src={icon} alt="denver-icon" style={{ width: '100px' }} /></a>
+            <nav className="navbar navbar-expand-lg p-2">
+                <div className="container-fluid">
+                    <div className="col-4">
+                        <a href="#"><img src={denver_icon} alt="denver-icon" style={{ width: '100px' }} /></a>
                     </div>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse col"  id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse col-4 justify-content-center" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Productos</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Productos</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Destacados</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Destacados</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled">Contacto</a>
+                            <li className="nav-item">
+                                <a className="nav-link disabled">Contacto</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="collapse navbar-collapse col-4 justify-content-end">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Facebook</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Instagram</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Twitter</a>
                             </li>
                         </ul>
                     </div>
